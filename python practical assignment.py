c96bd9833038512827f7032e2e8e3d366c4ea3bd
Jafar1089 <81628849+Jafar1089@users.noreply.github.com>
2021-04-02 15:53:22 -0700

    Add files via upload

---
+++ python practical assignment.py
@@ -0,0 +1,30 @@
+#Q1.
+name = "Jafar"
+dept = "computer science"
+print(name,"\n",dept,"\n")
+
+#Q2.
+scare = "boo!"
+print(scare, "\n")
+
+#Q3.
+n1 = 3
+n2 = 5
+n3 = n1 + n2
+print("n1 = ",n1,"\nn2 = ",n2,"\nn3 = n1 + n2", "\nn3 = ", n1+n2, "\n")
+
+#Q4. 
+rem = 8/0.5
+print(rem, "\n")
+
+#Q5.
+total = 4 + 5
+print(total, "\n")
+
+#Q6.
+rem = 8%3
+print(rem, "\n")
+
+#Q7.
+x = (2*4)*(4+2)
+print(x)
